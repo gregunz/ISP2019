a033c5f8682bf7af3ecc0c3963cebcd06d8d1d6c
Gregoire Clement <gregoire.clement@epfl.ch>
2019-03-17 19:48:05 +0100

    ex1 done and got token

--- HW2/Exercise01/ex1.py
+++ HW2/Exercise01/ex1.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import request, abort
+from flask import Flask, request, abort
 import base64
 import sys
 
@@ -31,3 +30,5 @@ def login():
         return 'success'
     else:
         return abort(400)
+
+app.run()
